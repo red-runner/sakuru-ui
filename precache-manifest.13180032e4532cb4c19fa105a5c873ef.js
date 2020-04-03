89b5ec608a810aadd0ac5ffd981769def1a7d02c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-03 14:47:50 +0000

    Deploy red-runner/sakuru-ui to github.com/red-runner/sakuru-ui.git:gh-pages

---
+++ precache-manifest.13180032e4532cb4c19fa105a5c873ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2668d6a1e6f03367fe453d04906a5490",
+    "url": "/red-runner/sakuru-ui/index.html"
+  },
+  {
+    "revision": "84962b324fec6349c6f6",
+    "url": "/red-runner/sakuru-ui/static/css/main.61b3694c.chunk.css"
+  },
+  {
+    "revision": "2e0e5aaf8f217fc385f9",
+    "url": "/red-runner/sakuru-ui/static/js/2.787859dc.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/red-runner/sakuru-ui/static/js/2.787859dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84962b324fec6349c6f6",
+    "url": "/red-runner/sakuru-ui/static/js/main.faa9ee73.chunk.js"
+  },
+  {
+    "revision": "18581edd15830a2effe0",
+    "url": "/red-runner/sakuru-ui/static/js/runtime-main.6ff1091d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/red-runner/sakuru-ui/static/media/logo.5d5d9eef.svg"
+  }
+]);
