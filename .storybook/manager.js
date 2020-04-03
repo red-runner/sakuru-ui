acc85f6dce0774570428fe50ee227dad4972ce82
red-runner <pol@lodgify.com>
2020-04-03 16:01:03 +0200

    feat(Storybook): add styles for theme

---
+++ .storybook/manager.js
@@ -0,0 +1,7 @@
+import { addons } from '@storybook/addons';
+import { themes } from '@storybook/theming';
+import { sakuruTheme } from './sakuruTheme';
+
+addons.setConfig({
+  theme: sakuruTheme,
+});
